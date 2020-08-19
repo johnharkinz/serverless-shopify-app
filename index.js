24ccf2c049b85dff0a4f01b8e526d1543119fef9
John <john.harkinz@gmail.com>
2020-08-19 19:51:52 +0100

    took out next js to try to get the middleware and shopify auth working

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const sls = require('serverless-http');
-const binaryMimeTypes = require('./binaryMimeTypes');
+const sls = require('serverless-http')
+const binaryMimeTypes = require('./binaryMimeTypes')
 
-const server = require('./server');
-console.log(server);
-console.log(typeof server.handle);
-module.exports.server = sls(server, { binary: binaryMimeTypes });
+const server = require('./server')
+
+module.exports.server = sls(server, {
+  binary: binaryMimeTypes
+})
